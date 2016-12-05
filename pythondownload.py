e344101aeea95bf0248799a715e1d8c8299b4b6c
sciencefreak500 <gkm24@drexel.edu>
2016-12-05 18:12:02 -0500

    First Push
    has the downloader file

---
+++ pythondownload.py
@@ -0,0 +1,26 @@
+import urllib3
+import time
+import os
+
+
+def downloadFile(url):
+	http = urllib3.PoolManager()
+	r = http.request('GET',url)
+	print("gettin")
+
+	if r.status == 404:
+		print("NOT FOUND")
+		return 0
+
+	if r.status == 200:
+		file = open(url.split('/')[-1],'wb')
+		file.write(r.data)
+		file.close()
+
+
+
+if os.name == 'nt':    #WINDOWS system
+	print('Using Windows System')
+	print('Downloading BOINC + Virtualbox')
+	downloadFile('https://boinc.berkeley.edu/dl/boinc_7.6.22_windows_x86_64_vbox.exe')
+	#download.gridcoin.us/download/downloadstake/GridcoinResearch.msi
